de0dc27c662b3b5127aa1095642c62b5325f9679
= <joakim.bostrom43@gmail.com>
2021-11-29 23:31:05 +0100

    lkol

--- Assets/Scripts/Player/BugAnimator.cs
+++ Assets/Scripts/Player/BugAnimator.cs
@@ -7,6 +7,8 @@ public class BugAnimator : MonoBehaviour
     Bone rootBone;
     public GameObject rootBoneGO;
 
+    public float turn = 0f;
+
     void Start()
     {
         rootBone = rootBoneGO.GetComponent<Bone>();    
@@ -14,8 +16,19 @@ public class BugAnimator : MonoBehaviour
 
     void Update()
     {
-        float rot = Input.GetAxis("Horizontal");
+        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        float goingAngle = FindDegree(dir.x, dir.y);
+        float currentAngle = transform.parent.parent.rotation.eulerAngles.z;
+        float diff = (goingAngle - currentAngle) / goingAngle;
+        // TODO this is not working lol
+        rootBone.Input(0);
+    }
 
-        rootBone.Input(rot);
+    public static float FindDegree(float x, float y)
+    {
+        float value = ((Mathf.Atan2(x, y) / Mathf.PI) * 180f);
+        value -= 360;
+        value = Mathf.Abs(value);
+        return value - 180;
     }
 }
